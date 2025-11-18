a5e18ca791b5b0d48f5dba14ca9e1dc7d2437744
stikbug <stikbuglol@outlook.com>
2025-11-18 11:32:01 -0600

    Changes: - Did dumb shit and neutered most of the mod template lol

---
+++ src/main/java/net/Iceforkkk/IcesAPI/IcesAPI.java
@@ -0,0 +1,33 @@
+package net.Iceforkkk.IcesAPI;
+
+import net.neoforged.fml.config.ModConfig;
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+@Mod(IcesAPI.MODID)
+public class IcesAPI {
+    public static final String MODID = "ices_api";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public IcesAPI(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+        NeoForge.EVENT_BUS.register(this);
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+}
--- src/main/java/net/Iceforkkk/IcesAPI/IcesAPIClient.java
+++ src/main/java/net/Iceforkkk/IcesAPI/IcesAPIClient.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package net.Iceforkkk.IcesAPI;
 
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
@@ -11,21 +11,21 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = ExampleMod.MODID, dist = Dist.CLIENT)
+@Mod(value = IcesAPI.MODID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = ExampleMod.MODID, value = Dist.CLIENT)
-public class ExampleModClient {
-    public ExampleModClient(ModContainer container) {
+@EventBusSubscriber(modid = IcesAPI.MODID, value = Dist.CLIENT)
+public class IcesAPIClient {
+    public IcesAPIClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
         // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
-        // Do not forget to add translations for your config options to the en_us.json file.
+        // Don't forget to add translations for your config options to the en_us.json file.
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
 
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
         // Some client setup code
-        ExampleMod.LOGGER.info("HELLO FROM CLIENT SETUP");
-        ExampleMod.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        IcesAPI.LOGGER.info("HELLO FROM CLIENT SETUP");
+        IcesAPI.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
 }
